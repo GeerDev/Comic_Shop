5a0705ccb21f692744e83092bef3401b34dc02e6
Gerahara <gerfersie@gmail.com>
2022-01-23 23:21:41 +0100

    Register user with Bcryptjs

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,7 +1,18 @@
 const { User } = require('../models/index.js');
+const bcrypt = require('bcryptjs');
 
 const UserController = {
-
+    async register(req, res) {
+        try {
+            const { password } = req.body
+            const hash = await bcrypt.hash( password, 10)
+            const newUser = await User.create({...req.body, password: hash, rol: 'user'})
+            res.status(201).send({ newUser })
+        } catch (error) {
+            console.error(error);
+            res.status(500).send({ error, message: 'Hubo un problema al tratar de registar' })
+        }
+    },
 }
 
 module.exports = UserController
